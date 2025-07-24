2f76dc32dc91da462f97850022d33292f3138278
tsotnemikadze <tsotnemikadze@gmail.com>
2025-07-24 22:43:40 +0200

    add aasertion

--- cli/src/test/java/tools/vitruv/cli/CLITest.java
+++ cli/src/test/java/tools/vitruv/cli/CLITest.java
@@ -1,11 +1,16 @@
 package tools.vitruv.cli;
 
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import java.io.File;
+
 import org.junit.jupiter.api.Test;
 
 class CLITest {
 
   @Test
   void test() {
+    String outputDirPath = "target/internal/";
     CLI.main(
         new String[] {
             "-m",
@@ -14,5 +19,8 @@ class CLITest {
             "-u", "default",
             "-r", "src/test/resources/consistency/templateReactions.reactions"
         });
+      File outputDir = new File(outputDirPath);
+      assertTrue(outputDir.exists() && outputDir.isDirectory(),
+            "Expected output directory '" + outputDirPath + "' to be created by CLI.");F
     }
 }
