004e058bf09c7ece57fed0debdbee005bb019712
Mohammadali Mirzaei <93537795+ma-mirzaei@users.noreply.github.com>
2026-03-05 12:29:11 +0100

    Refactor PrecheckAndMweIntegrationTest to improve file handling with updated imports

--- cli/src/test/java/tools/vitruv/cli/options/PrecheckAndMweIntegrationTest.java
+++ cli/src/test/java/tools/vitruv/cli/options/PrecheckAndMweIntegrationTest.java
@@ -4,7 +4,9 @@ import static org.junit.jupiter.api.Assertions.*;
 
 import com.google.inject.Injector;
 import java.io.InputStream;
-import java.nio.file.*;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.StandardCopyOption;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
